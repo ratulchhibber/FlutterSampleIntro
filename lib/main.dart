ac7138a97854db1de5bd20b27fd6192fb38cb82f
Ratul Chhibber <ratulchhibber@gmail.com>
2020-04-26 00:52:01 +0530

    Adding the implementation for showcase

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                  child: Text("Container 1")
+              ),
+              Container(
+                  color: Colors.teal,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.yellow,
+                        width: 100,
+                        height: 100,
+                      ),
+                      Container(
+                        color: Colors.green,
+                        width: 100,
+                        height: 100,
+                      )
+                    ],
+                  ),
+              ),
+              Container(
+                  color: Colors.blue,
+                  height: 100,
+                  width: 100,
+                  child: Text("Container 2")
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
